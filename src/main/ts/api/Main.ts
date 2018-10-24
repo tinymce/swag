b352b2703ae05f342a9b3e6b483e971622633fd4
Spocke <spocke@moxiecode.com>
2018-10-24 09:43:42 +0200

    removed the export patch logic and bumped major

--- src/main/ts/api/Main.ts
+++ src/main/ts/api/Main.ts
@@ -1,9 +1,7 @@
 import { nodeResolve } from './NodeResolve';
-import { patchExports } from './PatchExports';
 import { remapImports } from './RemapImports';
 
 export {
   nodeResolve,
-  patchExports,
   remapImports
 };
